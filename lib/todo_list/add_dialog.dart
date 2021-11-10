7bd861d999a381d64cb9dbaa1acef43c1bd8b862
Nabi Isakhanov <nabi.isakhanov@dataart.com>
2021-11-10 03:16:25 +0300

    Separate code into files

---
+++ lib/todo_list/add_dialog.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddTodoDialog extends StatelessWidget {
+  final TextEditingController _textFieldController = TextEditingController();
+  final Function _addTodoItem;
+
+  AddTodoDialog(this._addTodoItem, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Add a task to your list'),
+      content: TextField(
+        controller: _textFieldController,
+        decoration: const InputDecoration(hintText: 'Enter task here'),
+      ),
+      actions: <Widget>[
+        // add button
+        TextButton(
+          child: const Text('ADD'),
+          onPressed: () {
+            Navigator.of(context).pop();
+            _addTodoItem(_textFieldController.text);
+            _textFieldController.clear();
+          },
+        ),
+        // Cancel button
+        TextButton(
+          child: const Text('CANCEL'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        )
+      ],
+    );
+  }
+}
--- lib/todo_list/models/todo_item_model.dart
+++ lib/todo_list/models/todo_item_model.dart
@@ -1,16 +1,16 @@
-class TodoItem {
+class TodoItemModel {
   final String id;
   final String content;
   final String createdAt;
 
-  TodoItem(this.id, this.content, this.createdAt);
+  TodoItemModel(this.id, this.content, this.createdAt);
 
-  TodoItem.fromJson(Map<String, dynamic> json)
+  TodoItemModel.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         content = json['content'],
         createdAt = json['createdAt'];
 
   Map<String, dynamic> toJson() {
-    return {id: id, content: content, createdAt: createdAt};
+    return {'id': id, 'content': content, 'createdAt': createdAt};
   }
 }
